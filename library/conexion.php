658a9d3572a8d3383f0ea496b7ad4e4eacbbcf76
roylamilla <roylamilla652@gmail.com>
2025-06-02 11:56:14 -0500

    1

--- library/conexion.php
+++ library/conexion.php
@@ -5,5 +5,20 @@ class Conexion{
     public static function connect(){
         $mysql = new mysqli(BD_HOST, BD_USER, BD_PASSWORD, BD_NAME);
         $mysql->set_charset(BD_CHARSET);
+        date_default_timezone_set("America/Lima");
+        if (mysqli_connect_errno()) {
+            echo "Error de conexion:".mysqli_connect_errno();
+        }else {
+            echo "Conexion exitosa";
+        }
     }
 }
+
+ $mysql = new mysqli(BD_HOST, BD_USER, BD_PASSWORD, BD_NAME);
+        $mysql->set_charset(BD_CHARSET);
+        date_default_timezone_set("America/Lima");
+        if (mysqli_connect_errno()) {
+            echo "Error de conexion:".mysqli_connect_errno();
+        }else {
+            echo "Conexion exitosa";
+        }
