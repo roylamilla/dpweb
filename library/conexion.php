bd1e374aaa06d58f1972ce35bd9f693d99700a1a
roylamilla <roylamilla652@gmail.com>
2025-06-02 11:41:21 -0500

    1

--- library/conexion.php
+++ library/conexion.php
@@ -4,5 +4,6 @@ require_once "../config/config.php";
 class Conexion{
     public static function connect(){
         $mysql = new mysqli(BD_HOST, BD_USER, BD_PASSWORD, BD_NAME);
+        $mysql->set_charset(BD_CHARSET);
     }
 }
