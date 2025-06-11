bb707cc58da11919bc4af41ad1bf1f85dc84c740
roylamilla <roylamilla652@gmail.com>
2025-06-11 11:29:05 -0500

    11-06-2025

--- view/login.php
+++ view/login.php
@@ -11,7 +11,7 @@
             width: 300px;
             margin: 200px auto;
             padding: 50px;
-            border: 1px solid #ccc;
+            border: 1px solid white;
             border-radius: 5px;
         }
         label {
@@ -23,21 +23,24 @@
             width: 100%;
             padding: 8px;
             margin-bottom: 15px;
-            border: 1px solid #ccc;
+            border: 1px solid white;
             border-radius: 4px;
             box-sizing: border-box;
         }
+        
+
         input[type="submit"] {
-            background-color:rgb(22, 14, 171);
-            color: white;
+            background-color:rgb(255, 255, 255);
+            color: black;
             padding: 10px 15px;
             border: none;
             border-radius: 10px;
             cursor: pointer;
         }
         input[type="submit"]:hover {
-            background-color:rgb(227, 9, 9);
+            background-color:skyblue;
         }
+        
     </style>
 
 </head>
@@ -55,23 +58,37 @@
             background-position: center;
             height: 100vh;
             width: 100vw;
-        }
+            }
 
         </style>
+        
         <div class="col-md-12 col-sm-12 px-0 py-0 mx-0 my-0 row" style="height: 100%;">
             <br>
-            <center><p style="color:white;"> <strong>INGRESA TUS DATOS</strong> </p></center>
-            <form action="/login" method="post"> <!-- Reemplazar "/login" con la ruta donde se procesará el formulario -->
-         <label for="username"><strong style="color:white;">Nombre de usuario/Correo electrónico:</strong></label>
-         <input type="text" id="username" name="username" required>
+            <center><p style="color:white;"> <strong class="datos">INGRESA TUS DATOS</strong> </p></center>
+            <form action="login" method="post"> <!-- Reemplazar "/login" con la ruta donde se procesará el formulario -->
+                <center><p style="color:white;"> <strong class="sesion">INICIO DE SESION</strong> </p></center>
+            
+                <label for="username"><strong style="color:white;"> Usuario:</strong></label>
+                <input type="text" id="username" name="username" required style="background-color: transparent;">
 
-         <label for="password"><strong style="color:white;">Contraseña:</strong></label>
-         <input type="password" id="password" name="password" required>
+                <label for="password"><strong style="color:white;">Contraseña:</strong></label>
+                <input type="password" id="password" name="password" required style="background-color: transparent;">
 
-         <input type="submit" value="Iniciar sesión">
-         </form>
+                <input type="submit" value="Iniciar sesión">
+            </form>
         </div>
     </div>
-    
+    <style>
+        .sesion{
+            position: relative;
+            top: -20px;
+        }
+        .datos{
+            position: relative;
+            top: 200px;
+        }
+        
+        
+    </style>
 </body>
 </html>
