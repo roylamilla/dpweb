d697c2ba8c6e9621e370a824c3343be85683fdc9
E.N.D. <roylamilla652@gmail.com>
2025-11-04 08:27:08 -0500

    1

--- view/login.php
+++ view/login.php
@@ -1,247 +1,118 @@
-<!--<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-
-        form {
-            width: 300px;
-            margin: 200px auto;
-            padding: 50px;
-            border: 5px solid white;
-            border-radius: 5px;
-        }
-
-        label {
-            display: block;
-            margin-bottom: 5px;
-        }
-
-        input[type="text"],
-        input[type="password"] {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 15px;
-            border: 5px solid white;
-            border-radius: 4px;
-            box-sizing: border-box;
-        }
-
-
-        input[type="submit"] {
-            background-color: rgb(255, 255, 255);
-            color: black;
-            padding: 10px 15px;
-            border: none;
-            border-radius: 10px;
-            cursor: pointer;
-        }
-
-        input[type="submit"]:hover {
-            background-color: skyblue;
-        }
-    </style>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            background-image: url('view/img/login.jpg');
-            /* Reemplaza con la URL de tu imagen */
-            background-size: cover;
-            background-position: center;
-            height: 100vh;
-            width: 100vw;
-        }
-    </style>
-    <style>
-        .sesion {
-            position: relative;
-            top: -20px;
-        }
-
-        .datos {
-            position: relative;
-            top: 200px;
-        }
-
-        input[type="text"] {
-            color: white;
-        }
-
-        input[type="password"] {
-            color: white;
-        }
-    </style>
-
-    <script>
-        const base_url = '<?php echo BASE_URL; ?>';
-    </script>
-
-</head>
-
-<body>
-
-    <center>
-        <div class="container-fluid row px-0 py-0 mx-0 my-0 ">
-
-
-            <div class="col-md-12 col-sm-12 px-0 py-0 mx-0 my-0 row" style="height: 100%;">
-                <br>
-                <center>
-                    <p style="color:white;"> <strong class="datos">INGRESA TUS DATOS</strong> </p>
-                </center>
-                <form id="frm_login">
-                    <center>
-                        <p style="color:white;"> <strong class="sesion">INICIO DE SESION</strong> </p>
-                    </center>
-
-                    <label for="username"><strong style="color:white;"> Usuario:</strong></label>
-                    <input type="text" id="username" name="username" required style="background-color: transparent;">
-
-                    <label for="password"><strong style="color:white;">Contraseña:</strong></label>
-                    <input type="password" id="password" name="password" required style="background-color: transparent">
-
-                    <button type="button" onclick="iniciar_sesion();">Ingresar</button>
-                </form>
-            </div>
-        </div>
-
-    </center>
-
-    <script src="<?php echo BASE_URL; ?>view/funtion/user.js"></script>
-
-
-
-</body>
-
-</html>
--->
-
 
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="boxicons/css/boxicons.css">
     <title>Login</title>
-    <!--<link rel="stylesheet" href="cl-icon/css/all.min.css">-->
     <style>
         * {
-            margin: 0;
-            padding: 0;
-            font-family: arial;
-            color: #fff;
+            font-family: 'Poppins', sans-serif;
         }
 
         body {
-            width: 100vw;
-            height: 100vh;
-            background: #081b29;
-            display: grid;
+            background-image: url("view/img/log.avif");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-attachment: fixed;
+        }
+
+        .box {
+            display: flex;
             justify-content: center;
-            align-content: center;
+            align-items: center;
+            min-height: 90vh;
+        }
+
+        .container {
+            width: 350px;
+            display: flex;
+            flex-direction: column;
+            padding: 0 15px 0 15px;
+        }
+
+        span {
+            color: #fff;
+            font-size: small;
+            display: flex;
+            justify-content: center;
+            padding: 10px 0 15px 0;
+        }
+
+        header {
+            color: #fff;
+            font-size: 30px;
+            display: flex;
+            justify-content: center;
+            padding: 0 0 15px 0;
+        }
+
+        .input-field {
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 15px;
+        }
+
+        .input-field .input {
+            height: 45px;
+            width: 87%;
+            border: none;
+            outline: none;
+            border-radius: 30px;
+            color: #fff;
+            padding: 0 0 0 42px;
+            background: rgba(255, 255, 255, 0.15);
+        }
+
+        i {
+            position: relative;
+            top: -31px;
+            left: 17px;
+            color: #fff;
         }
 
         ::-webkit-input-placeholder {
-            color: #eee;
-        }
-
-        .wrapper {
-            position: relative;
-            width: 800px;
-            height: 65vh;
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            border: 3px solid #00ffff;
-            box-shadow: 0 0 50px 0 #00a6bc;
-        }
-
-        .form {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .title {
-            font-size: 35px;
-        }
-
-        .inp {
-            padding-bottom: 10px;
-            border-bottom: 2px solid #eee;
-        }
-
-        .input {
-            border: none;
-            outline: none;
-            background: none;
-            width: 260px;
-            margin-top: 40px;
-            padding-right: 10px;
-            font-size: 17px;
-            color: #0ef;
-
+            color: #fff;
         }
 
         .submit {
             border: none;
+            border-radius: 30px;
+            font-size: 15px;
+            height: 45px;
             outline: none;
-            width: 288px;
-            margin-top: 25px;
-            padding: 10px 0;
-            font-size: 20px;
-            border-radius: 40px;
-            letter-spacing: 1px;
+            width: 100%;
+            background: rgba(255, 255, 255, 0.63);
             cursor: pointer;
-            background: linear-gradient(45deg, #0ef, #c800ff);
+            transition: .3s;
         }
 
-        .footer {
-            margin-top: 30px;
-            letter-spacing: 0.5px;
-            font-size: 14px;
+        .submit:hover {
+            background-color: #ffffffff;
+            box-shadow: 1px 5px 7px 1px rgba(0, 0, 0, 0.2);
         }
 
-        .link {
-            color: #0ef;
-            text-decoration: none;
-        }
-
-        .banner {
-            position: absolute;
-            top: 0;
-            right: 0;
-            width: 450px;
-            height: 390px;
-            background: linear-gradient(to right, #0ef, #c800ff);
-            clip-path: polygon(0 0, 100% 0, 100% 100%, 60% 100%);
-            padding-right: 70px;
-            text-align: right;
+        .bottom {
             display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: flex-end;
-        }
-
-        .wel_text {
-            font-size: 40px;
-            margin-top: -50px;
-            line-height: 50px;
-        }
-
-        .para {
+            flex-direction: row;
+            justify-content: space-between;
+            font-size: small;
+            color: #fff;
             margin-top: 10px;
-            font-size: 18px;
-            line-height: 24px;
-            letter-spacing: 1px;
+        }
+
+        .left {
+            display: flex;
+        }
+
+        label a {
+            color: #fff;
+            text-decoration: none;
         }
     </style>
     <script>
@@ -250,28 +121,32 @@
 </head>
 
 <body>
-    <div class="wrapper">
-        <form id="frm_login" class="form">
-            <h1 class="title">Inicio</h1>
-            <div class="inp">
-                <input type="text" name="username" id="username" class="input" placeholder="usuario">
-                <i class="fa-solid fa-user"></i>
+    <div class="box">
+        <div class="container">
+            <div class="top-header">
+                <span>Login</span>
+                <header>Iniciar Sesión</header>
             </div>
-            <div class="inp">
-                <input type="password" name="password" id="password" class="input" placeholder="contraseña">
-                <i class="fa-solid fa-lock"></i>
-            </div>
-            <!--<button class="submit" onclick="iniciar_sesion();">Iniciar sesión</button>-->
-            <button class="submit" type="button" onclick="iniciar_sesion();">Ingresar</button>
-            <p class="footer">¿No tienes cuenta? <a href="#" class="link">Por favor, Registrate</a></p>
-        </form>
-        <div></div>
-        <div class="banner">
-            <h1 class="wel_text">Bienvenid@</h1><br>
-            <p class="para"></p>
+
+            <form id="frm_login">
+                <div class="input-field">
+                    <input type="text" id="username" name="username" required class="input" placeholder="Usuario"
+                        required>
+                    <i class="bx bx-user"></i>
+                </div>
+                <div class="input-field">
+                    <input type="password" id="password" name="password" required class="input" placeholder="Contraseña"
+                        required>
+                    <i class="bx bx-lock-alt"></i>
+                </div>
+                <div class="input-field">
+                    <input type="button" class="submit" onclick="iniciar_sesion();" value="Inicio">
+                </div>
+
+            </form>
+
         </div>
     </div>
-
     <script src="<?php echo BASE_URL; ?>view/funtion/user.js"></script>
 </body>
 
